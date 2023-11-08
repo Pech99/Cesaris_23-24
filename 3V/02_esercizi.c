88cb58dc264f9c49b8b8f94ce87322e4b158b289
Pech99 <vittorio1999@live.com>
2023-11-08 10:22:13 +0100

    esempi

--- 3V/02_esercizi.c
+++ 3V/02_esercizi.c
@@ -1,11 +1,19 @@
+// Il codice scritto durante le lezioni viene pubblicato su
+// https://github.com/Pech99/Cesaris_23-24/
+
 /*
     Scrivere un programma che presi in input 10 valori,
     Stampi a schermo il massimo dei 10.
+
+    Esempio di input:   7 5 6 -6 0 -4 56 8 7 9 
+    Output Atteso:      Max: 56;
+
 */
 
 /*
-    Scrivere un programma che presi in input una serie di valori terminata da 0,
-    Stampi a schermo sia il massimo che il minimo. (lo 0 dev'essere escluso)
+    Scrivere un programma che presi in input una serie di
+    valori terminata da 0, Stampi a schermo sia il massimo
+    che il minimo. (lo 0 dev'essere escluso)
 
     Esempio di sequenza:    56 -2 3 22 5 -6 7 -15 9 0
     Output Atteso:          Max: 56;    Min: -15;
@@ -13,6 +21,12 @@
 
 /*
     Stampare un quadrato di '#' di lato preso in input
+    n = 5;
+    #####  |
+    #####  |
+    #####  | n
+    #####  |
+    #####  |
 */
 
 /*
@@ -23,5 +37,4 @@
       n | +++++
         |  +++
         |   +
-
 */
