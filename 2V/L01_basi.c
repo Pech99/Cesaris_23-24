2d5b7e01148a0703a9bb0dd3ba248026a1b12f59
Pech99 <vittorio1999@live.com>
2023-11-14 14:30:38 +0100

    C basi 2V

---
+++ 2V/L01_basi.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+
+/*
+	tutto il codice scritto verrà caricato all'indirizzo:
+	https://github.com/Pech99/Cesaris_23-24/tree/main/2V
+*/
+
+
+// { = SHIFT + ALT GR + [
+int main(){
+	int num1 = 1;
+	int num2 = 8;
+	
+	num1 = num1 + 5;
+	num1 = num1 + num2;
+	
+	//stampa a schermo una stringa di carattetri
+	printf("%d", num1);
+	
+	//legge un input intero
+	scanf("%d", &num1);
+
+}
